8b0b0d0575a576cc6cd22ba88a050fd032bb8f5a
mlampedx <mlampesd@gmail.com>
2017-01-20 17:49:17 -0800

    Added MuiThemeProvider

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import Layout from './layout';
 import Profile from './profile';
 import HostApp from './hostapp';
@@ -14,17 +15,20 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path="/" component={Home} >
-          {/* make them childen of `App` */}
-          <IndexRoute component={Profile} />
-          <Route path="profile" component={Profile} newState={this.newState}/>
-          <Route path="host/:eventId" component={HostApp} state={this.state} newState={this.newState}/>
-          <Route path="guest/:eventId" component={GuestApp} state={this.state} newState={this.newState}/>
-          <Route path="createevent" component={CreateEvent} state={this.state} newState={this.newState} />
-        </Route>
-      </Router>)
-    }
+      <MuiThemeProvider>
+        <Router history={hashHistory}>
+          <Route path="/" component={Home} >
+            {/* make them childen of `App` */}
+            <IndexRoute component={Profile} />
+            <Route path="profile" component={Profile} newState={this.newState}/>
+            <Route path="host/:eventId" component={HostApp} state={this.state} newState={this.newState}/>
+            <Route path="guest/:eventId" component={GuestApp} state={this.state} newState={this.newState}/>
+            <Route path="createevent" component={CreateEvent} state={this.state} newState={this.newState} />
+          </Route>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
     getData() {
       $.get(HOST + "/queue").done((data) => {
         this.setState({songs: data});
